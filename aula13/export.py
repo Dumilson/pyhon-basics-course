bab786e62d7f421928f0e7af3221012564657365
Domingos Braganha <domingosbreganha9@gmail.com>
2023-05-06 12:46:57 +0100

    feat: using libs and import libs

---
+++ aula13/export.py
@@ -0,0 +1,9 @@
+PI = 2.14
+GV = 9.8
+
+
+def get_extension(file):
+    return file[file.index(".") + 1:]
+
+def highest_number(numbers):
+    return max(numbers)
---
+++ aula13/modules.py
@@ -0,0 +1,12 @@
+#Modulos são bibliotecas ou funções reutilizaveis 
+#pip gerenciador de tarefas 
+
+#para instalar o pacote de form global pip install nome_pacote
+#para instalar na pasta do projecto pip install nome_pacote .t /pasta_salvar
+#para desinstalar pip unintsall nome_pacote 
+
+import export as tools
+# import 
+
+print(tools.get_extension("text.php"))
+print(tools.highest_number([10,89,90]))
